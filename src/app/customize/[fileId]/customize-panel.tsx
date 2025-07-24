cc9fab7d4ce3ccd73cb55fecc58eab1823d558d2
Shorya Dubey <shoryadubey7610@gmail.com>
2025-07-24 18:57:30 +0530

    fix more responsiveness.

--- src/app/customize/[fileId]/customize-panel.tsx
+++ src/app/customize/[fileId]/customize-panel.tsx
@@ -96,7 +96,7 @@ export function CustomizePanel({
         </div>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div className="w-full max-w-screen-xl mx-auto px-4 grid grid-cols-1 md:grid-cols-2 gap-8">
         <div className="space-y-4">
           <div>
             <Card className="p-4 space-y-4">
